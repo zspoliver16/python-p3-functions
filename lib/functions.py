76cf9d11bf98c53c24c553184c3bc7a1cdbe55dd
zachary oliver <zspoliver@gmail.com>
2024-05-14 10:57:31 -0400

    adds p3 functions lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
